4925dc4216992f37fe772710a63f1a95be8ae95f
mrbruce516 <mrbruce516>
2022-04-22 16:31:17 +0800

    update

--- getTV.sh
+++ getTV.sh
@@ -2,6 +2,8 @@
 
 # 筛选范围
 want_tv=(HK MO TW JP)
+blocklist=(blocked "GOOD TV CH")
+m3ulist=(hk.m3u mo.m3u tw.m3u jp.m3u)
 
 # 个人环境
 workdir="/home/bruce/Developer/GetTV"
@@ -9,23 +11,28 @@ pushdir="/data/nginx/index"
 jellyfin="/data/jellyfin/media/TV"
 
 pull(){
-    wget https://iptv-org.github.io/iptv/countries/hk.m3u
-    wget https://iptv-org.github.io/iptv/countries/mo.m3u
-    wget https://iptv-org.github.io/iptv/countries/tw.m3u
-    wget https://iptv-org.github.io/iptv/countries/jp.m3u
+    for((i=0;i<=${#m3ulist[@]}-1;i++))
+    do
+        wget https://iptv-org.github.io/iptv/countries/${m3ulist[i]}
+    done
 }
 
 search(){
-    j=(hk.m3u mo.m3u tw.m3u jp.m3u)
-    for((k=0;k<=${#j[@]}-1;k++))
+    for((k=0;k<=${#m3ulist[@]}-1;k++))
     do
         for((i=0;i<=${#want_tv[@]}-1;i++))
         do
-            grep -A 1 "${want_tv[i]}" ${j[k]}
+            grep -A 1 "${want_tv[i]}" ${m3ulist[k]}
         done
     done
 }
-#search > iptv.m3u
+
+block(){
+    for((i=0;i<=${#blocklist[i]}-1;i++))
+    do
+        sed -i "/${blocklist[i]}/,+1d" iptv.m3u
+    done
+}
 
 main(){
     cd $workdir
@@ -34,25 +41,22 @@ main(){
     if [ $? -eq 1 ]
     then
         echo "下载文件失败，请检查网络" >> /home/bruce/Desktop/getTV.log
-    fi
-    search > $pushdir/iptv.m3u
-    sed -i '/blocked/,+1d' $pushdir/iptv.m3u
-    search > $jellyfin/iptv.m3u
-    sed -i '/blocked/,+1d' $jellyfin/iptv.m3u
-    if [ $? -eq 0 ]
-    then
-        if [ `wc -l /home/bruce/Desktop/getTV.log|awk '{print $1}'` -gt 12 ]
-        then
-            rm /home/bruce/Desktop/getTV.log
-        fi
-        echo "更新m3u文件成功 `date "+%Y-%m-%d %H:%M:%S"`" >> /home/bruce/Desktop/getTV.log
-    else
-        if [ `wc -l /home/bruce/Desktop/getTV.log|awk '{print $1}'` -gt 12 ]
-        then
-            rm /home/bruce/Desktop/getTV.log
-        fi
-        echo "更新文件失败，请检查源文件 `date "+%Y-%m-%d %H:%M:%S"`" >> /home/bruce/Desktop/getTV.log
         exit 1
+    fi
+    search > iptv.m3u
+    block
+    if [ $? -eq 1 ]
+    then
+        echo "筛选节目失败，请检查原始m3u文件" >> /home/bruce/Desktop/getTV.log
+        exit 1
+    fi
+    cp iptv.m3u $pushdir
+    cp iptv.m3u $jellyfin
+    if [ `wc -l /home/bruce/Desktop/getTV.log|awk '{print $1}'` -gt 12 ]
+    then
+        echo "更新m3u文件成功 `date "+%Y-%m-%d %H:%M:%S"`" > /home/bruce/Desktop/getTV.log
+    else
+        echo "更新m3u文件成功 `date "+%Y-%m-%d %H:%M:%S"`" >> /home/bruce/Desktop/getTV.log
     fi
 }
 
